6a696c1ae24e53a1410ed5ca51f311226645f166
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-02 18:59:30 +0800

    初始化

---
+++ newadmin/src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from "axios";
+
+// 绑定到原型
+Vue.prototype.$axios = axios;
+
+//设置默认的 api 域名
+axios.defaults.baseURL = "http://127.0.0.1:3000"
+Vue.config.productionTip = false
+// 引入 ui 库
+import ElementUI from 'element-ui';
+// 引入 css
+import 'element-ui/lib/theme-chalk/index.css';
+//注册组件库
+Vue.use(ElementUI);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
