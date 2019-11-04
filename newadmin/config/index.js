c6f6fd6b4d516e0f4a71899e6834f421c2815832
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-04 11:30:55 +0800

    修改打包配置

--- newadmin/config/index.js
+++ newadmin/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/newsadmin_dev/',
     proxyTable: {},
 
     // Various Dev Server settings
