8249851d2f28045ba7f52eb55900294fea591346
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-04 17:10:58 +0800

    完成类型

--- newadmin/src/pages/EditPost.vue
+++ newadmin/src/pages/EditPost.vue
@@ -3,7 +3,7 @@
     <el-form-item label="标题">
       <el-input v-model="form.title"></el-input>
     </el-form-item>
-    <el-form-item label="类型">
+    <el-form-item label="栏目">
       <el-checkbox-group v-model="form.categories">
         <el-checkbox 
         v-for="(item,index) in categoryList" :key="index"
@@ -12,6 +12,13 @@
         </el-checkbox>
         </el-checkbox-group>
     </el-form-item>
+    <el-form-item label="类型">
+            <el-radio v-model="form.type" label="1">文章</el-radio>
+            <el-radio v-model="form.type" label="2">视频</el-radio>
+        </el-form-item>
+      <el-form-item>
+            <el-button type="primary" @click="onSubmit">提交</el-button>
+        </el-form-item>
   </el-form>
 </template>
 
@@ -21,7 +28,8 @@ export default {
     return {
       form: {
         title: "",
-        categories:[]
+        categories:[],
+        type:1
       },
       categoryList:[]
     };
@@ -35,7 +43,14 @@ export default {
       const {data} =res.data;
       this.categoryList=data;
     })
-  }
+  },
+    methods: {
+        onSubmit() {
+            // 这里是提交的函数,但是我们现在还没有完整的数据,所以只是打印一下数据确认我们的操作
+            console.log(this.form);
+            
+        }
+    }
 };
 </script>
 <style lang="less" scoped>
