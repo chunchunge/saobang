f21cfddbad36a1fd07c50539235d852082d142d8
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-04 11:58:28 +0800

    测试

--- newadmin/config/index.js
+++ newadmin/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/saobang/',
+    assetsPublicPath: '/newadmin/saobang/',
     proxyTable: {},
 
     // Various Dev Server settings
