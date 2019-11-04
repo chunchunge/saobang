b361b79a38b3a8ffc4357ef709d329b39a8778cc
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-04 11:43:25 +0800

    测试

--- newadmin/config/index.js
+++ newadmin/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/newsadmin_dev/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
