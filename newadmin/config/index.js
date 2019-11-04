36c0ef0e6dd2cbb968190ad2265a88172926d9e7
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-04 20:01:50 +0800

    打包01

--- newadmin/config/index.js
+++ newadmin/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/saobang',
+    assetsPublicPath: '/saobang/',
     proxyTable: {},
 
     // Various Dev Server settings
