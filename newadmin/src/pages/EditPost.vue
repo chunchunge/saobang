227e731c82745d2424dc9e0e52c82ddbbb96d755
qiangyanhuanxiao <5401587+chunchunge@user.noreply.gitee.com>
2019-11-05 19:16:01 +0800

    编辑器中图片上传完成

--- newadmin/src/pages/EditPost.vue
+++ newadmin/src/pages/EditPost.vue
@@ -5,84 +5,109 @@
     </el-form-item>
     <el-form-item label="栏目">
       <el-checkbox-group v-model="form.categories">
-        <el-checkbox 
-        v-for="(item,index) in categoryList" :key="index"
-        :label="item.id">
-          {{item.name}}
-        </el-checkbox>
-        </el-checkbox-group>
+        <el-checkbox
+          v-for="(item,index) in categoryList"
+          :key="index"
+          :label="item.id"
+        >{{item.name}}</el-checkbox>
+      </el-checkbox-group>
     </el-form-item>
     <el-form-item label="类型">
-            <el-radio v-model="form.type" label="1">文章</el-radio>
-            <el-radio v-model="form.type" label="2">视频</el-radio>
-        </el-form-item>
-         <el-form-item>
-            <vue-editor 
-            v-model="form.content"
-            :useCustomImageHandler="true"
-            @image-added="imgUpload"
-            :editorToolbar="customToolbar"
-            ></vue-editor>
-        </el-form-item>
-      <el-form-item>
-            <el-button type="primary" @click="onSubmit">提交</el-button>
-        </el-form-item>
+      <el-radio v-model="form.type" label="1">文章</el-radio>
+      <el-radio v-model="form.type" label="2">视频</el-radio>
+    </el-form-item>
+    <el-form-item>
+      <vue-editor
+        v-model="form.content"
+        :useCustomImageHandler="true"
+        @image-added="imgUpload"
+        :editorToolbar="customToolbar"
+      ></vue-editor>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">提交</el-button>
+    </el-form-item>
   </el-form>
 </template>
 
 <script>
 import { VueEditor } from "vue2-editor";
 export default {
-   components: {
-        VueEditor
-    },
+  components: {
+    VueEditor
+  },
   data() {
     return {
       form: {
         title: "",
-        categories:[],
-        type:"",
-         content: '<h1>没有内容</h1>'
+        categories: [],
+        type: "",
+        content: "<h1>没有内容</h1>"
       },
-      categoryList:[],
-       customToolbar: [["bold", "italic", "underline"], [{ list: "ordered" }, { list: "bullet" }], ["image", "code-block"]]
-        }
-    
+      categoryList: [],
+      customToolbar: [
+        ["bold", "italic", "underline"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        ["image", "code-block"]
+      ]
+    };
   },
-  mounted(){
+  mounted() {
     // 获取所有数据
     this.$axios({
-      url:'/category',
-      method:'get'
-    }).then(res=>{
-      const {data} =res.data;
+      url: "/category",
+      method: "get"
+    }).then(res => {
+      const { data } = res.data;
       // 这里我们需要过滤掉关注和头条
-      const newCategoryList=[];
-      data.forEach(element=>{
-        if(element.id !=0 && element.id !=999){
+      const newCategoryList = [];
+      data.forEach(element => {
+        if (element.id != 0 && element.id != 999) {
           newCategoryList.push(element);
         }
-      })
-      this.categoryList=newCategoryList;
-    })
+      });
+      this.categoryList = newCategoryList;
+    });
   },
-    methods: {
-        onSubmit() {
-            // 这里是提交的函数,但是我们现在还没有完整的数据,所以只是打印一下数据确认我们的操作
-           const newArr=[];
-           this.form.categories.forEach(element => {
-             newArr.push({
-               id:element
-             })
-           });
-           this.form.categories=newArr;
-               console.log(this.form);
-        },
-         // 这里是富文本框添加图片触发的函数
-        imgUpload() {
-            console.log('添加了图片');
-        }
+  methods: {
+    onSubmit() {
+      // 这里是提交的函数,但是我们现在还没有完整的数据,所以只是打印一下数据确认我们的操作
+      const newArr = [];
+      this.form.categories.forEach(element => {
+        newArr.push({
+          id: element
+        });
+      });
+      this.form.categories = newArr;
+      console.log(this.form);
+    },
+    // 这里是富文本框添加图片触发的函数
+    imgUpload(file, Editor, cursorLocation, resetUploader) {
+      // file 使我们要上传的文件
+      // Editor 是我们正在使用的编辑器
+      // cursorLocation 这是子组件获取到的当前光标位置
+      // restUploader 这是当你处理完了所有数据,应该重置这个上传的组件
+      var formData = new FormData();
+      formData.append("file", file);
+
+      this.$axios({
+        url: "/upload",
+        method: "POST",
+        data: formData
+      })
+        .then(res => {
+          console.log(res);
+          const { data } = res.data;
+          let url = this.$axios.defaults.baseURL + data.url;
+          // url 使我们在上面传输完图片获取的地址
+          Editor.insertEmbed(cursorLocation, "image", url);
+          resetUploader();
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
+  }
 };
 </script>
 <style lang="less" scoped>
